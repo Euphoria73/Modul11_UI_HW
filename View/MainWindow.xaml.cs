3b181d517add68b76243fb85d71a7d7067044c5d
Euphoria73 <miky-sundukov@yandex.ru>
2021-03-04 15:32:53 +0400

    Поправил ошибку запуска приложения

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Collections.ObjectModel;
 
-namespace Modul11_UI_HW.View
+namespace Modul11_UI_HW
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
